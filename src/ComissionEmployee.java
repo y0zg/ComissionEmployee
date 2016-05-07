b937330386ddce66bed562781822b76accb8a24a
michmauzer004 <michmauzer004@gmail.com>
2016-05-07 14:22:22 +0300

    Initial commit

---
+++ src/ComissionEmployee.java
@@ -0,0 +1,74 @@
+/**
+ * Created by webstddesign on 07.05.2016.
+ */
+public class ComissionEmployee extends Object {
+
+	private final String firstName;
+	private final String lastName;
+	private final String socialSecurityNumber;
+	private double grossSales;
+	private double comissionRate;
+
+	public ComissionEmployee
+			(String firstName,String lastName, String socialSecurityNumber, double grossSales,double comissionRate)
+	{
+		if (grossSales<0.0){
+			throw new IllegalArgumentException("Gross sales must be > 0");
+		}
+
+		if (comissionRate<=0.0 || comissionRate>=1.0) {
+			throw new IllegalArgumentException("Comission rate must be >0 and <1");
+		}
+
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.socialSecurityNumber=socialSecurityNumber;
+		this.grossSales=grossSales;
+		this.comissionRate=comissionRate;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getSocialSecurityNumber() {
+		return socialSecurityNumber;
+	}
+
+	public void setGrossSales(double grossSales) {
+
+		if(grossSales<0.0) {
+			throw new IllegalArgumentException("Gross must be >0.0");
+		}
+
+		this.grossSales = grossSales;
+	}
+
+	public double getGrossSales() {
+		return grossSales;
+	}
+
+	public void setComissionRate(double comissionRate) {
+	if (comissionRate<0.0 || comissionRate>1.0) {
+		throw new IllegalArgumentException("ComissionRate must be > 0.0 or < 1.0");
+	}
+			this.comissionRate = comissionRate;
+	}
+
+	public double getComissionRate() {
+		return comissionRate;
+	}
+
+	public double earnings(){
+		return comissionRate*grossSales;
+	}
+
+	public String toString(){
+		return String.format(firstName,lastName,socialSecurityNumber,grossSales,comissionRate);
+	}
+
+}
---
+++ src/ComissionEmployeeTest.java
@@ -0,0 +1,27 @@
+/**
+ * Created by webstddesign on 07.05.2016.
+ */
+public class ComissionEmployeeTest {
+	public static void main(String arg[]){
+		ComissionEmployee employee=new ComissionEmployee("a","a","a",400,0.4);
+		System.out.printf("%n%s %s%n", "First name is",
+				employee.getFirstName());
+		System.out.printf("%s %s%n", "Last name is",
+				employee.getLastName());
+		System.out.printf("%s %s%n", "Social security number is",
+				employee.getSocialSecurityNumber());
+		System.out.printf("%s %.2f%n", "Gross sales is",
+				employee.getGrossSales());
+		System.out.printf("%s %.2f%n", "Commission rate is",
+				employee.getComissionRate());
+		System.out.printf("%s %.2f%n", "Earnings rate is",
+				employee.earnings());
+
+		employee.setGrossSales(5000);
+		employee.setComissionRate(.1);
+
+		System.out.printf("%n%s:%n%n%s%n",
+				"Updated employee information obtained by toString", employee.earnings());
+
+	}
+}
