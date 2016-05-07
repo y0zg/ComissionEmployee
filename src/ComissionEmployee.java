bad655f0f827335f2dcaffbf13849273bedeb34a
michmauzer004 <michmauzer004@gmail.com>
2016-05-07 19:53:02 +0300

    TesttoStringClass

--- src/ComissionEmployee.java
+++ src/ComissionEmployee.java
@@ -10,21 +10,20 @@ public class ComissionEmployee extends Object {
 	private double comissionRate;
 
 	public ComissionEmployee
-			(String firstName,String lastName, String socialSecurityNumber, double grossSales,double comissionRate)
-	{
-		if (grossSales<0.0){
+			(String firstName, String lastName, String socialSecurityNumber, double grossSales, double comissionRate) {
+		if (grossSales < 0.0) {
 			throw new IllegalArgumentException("Gross sales must be > 0");
 		}
 
-		if (comissionRate<=0.0 || comissionRate>=1.0) {
+		if (comissionRate <= 0.0 || comissionRate >= 1.0) {
 			throw new IllegalArgumentException("Comission rate must be >0 and <1");
 		}
 
-		this.firstName=firstName;
-		this.lastName=lastName;
-		this.socialSecurityNumber=socialSecurityNumber;
-		this.grossSales=grossSales;
-		this.comissionRate=comissionRate;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.socialSecurityNumber = socialSecurityNumber;
+		this.grossSales = grossSales;
+		this.comissionRate = comissionRate;
 	}
 
 	public String getFirstName() {
@@ -41,7 +40,7 @@ public class ComissionEmployee extends Object {
 
 	public void setGrossSales(double grossSales) {
 
-		if(grossSales<0.0) {
+		if (grossSales < 0.0) {
 			throw new IllegalArgumentException("Gross must be >0.0");
 		}
 
@@ -53,22 +52,27 @@ public class ComissionEmployee extends Object {
 	}
 
 	public void setComissionRate(double comissionRate) {
-	if (comissionRate<0.0 || comissionRate>1.0) {
-		throw new IllegalArgumentException("ComissionRate must be > 0.0 or < 1.0");
-	}
-			this.comissionRate = comissionRate;
+		if (comissionRate < 0.0 || comissionRate > 1.0) {
+			throw new IllegalArgumentException("ComissionRate must be > 0.0 or < 1.0");
+		}
+		this.comissionRate = comissionRate;
 	}
 
 	public double getComissionRate() {
 		return comissionRate;
 	}
 
-	public double earnings(){
-		return comissionRate*grossSales;
+	public double earnings() {
+		return comissionRate * grossSales;
 	}
 
-	public String toString(){
-		return String.format(firstName,lastName,socialSecurityNumber,grossSales,comissionRate);
+	@Override
+	public String toString() {
+		return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f",
+				"commission employee", firstName, lastName,
+				"social security number", socialSecurityNumber,
+				"gross sales", grossSales,
+				"commission rate", comissionRate);
 	}
 
 }
